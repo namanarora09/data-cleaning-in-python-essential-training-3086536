79525a5f77d0b5d570b493de26bc2274953c7f65
NAMAN ARORA <88842004+namanarora09@users.noreply.github.com>
2023-03-04 07:48:27 +0000

    challenge task

--- Ch01/01_03/bad_values.py
+++ Ch01/01_03/bad_values.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # %%
-df = pd.read_csv('metrics.csv', parse_dates=['time'])
+df = pd.read_csv('../01_03/metrics.csv', parse_dates=['time'])
 df.sample(10)
 
 # %%
@@ -17,9 +17,11 @@ pd.pivot(df, index='time', columns='name').plot(subplots=True)
 # %%
 df.query('name == "cpu" & (value < 0 | value > 100)')
 
-# %% 
+# %%
 mem = df[df['name'] == 'mem']['value']
 z_score = (mem - mem.mean())/mem.std()
 bad_mem = mem[z_score.abs() > 2]
 # bad_mem
 df.loc[bad_mem.index]
+
+# %%
